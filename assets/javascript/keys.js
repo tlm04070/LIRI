32f28dc59424d59c97bb4a3a9452fe1c194fc491
Trevor Meadows <tlm04070@gmail.com>
2017-10-30 22:11:08 -0400

    added keys and vars in liri

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here>',
-    access_token_secret: '<input here>',
+    consumer_key: 'Vpxj78eFxZiZBePwVgdjIbFzJ',
+    consumer_secret: 'efvJW4CBR4CEDEwfUq2XhfArFO0KXYxmJHKWCAX9HStGPq2hr2',
+    access_token_key: '818980823051407360-PwQ6AtxZUe6v3mbZwfjqBfRlAyjgk0D',
+    access_token_secret: 'zy7DYmxt2k0UGzzsammEhQ1XONtaGxk7O321T1i8kRP0z',
 }
 
 module.exports = twitterKeys;
---
+++ assets/javascript/liri.js
@@ -0,0 +1,6 @@
+const twitterKeys = require('./keys.js');
+const consumerKey = twitterKeys.consumer_key;
+const consumerSecret = twitterKeys.consumer_secret;
+const accessKey = twitterKeys.access_token_key;
+const accessSecret = twitterKeys.access_token_secret;
+const argv = process.argv[2];
