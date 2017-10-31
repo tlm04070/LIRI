d2bdbd5089ab5574a56f9caf55fc2d2f5078fc39
Trevor Meadows <tlm04070@gmail.com>
2017-10-30 22:41:27 -0400

    working on twitter api calls

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,6 +1,14 @@
+const request = require('request');
 const twitterKeys = require('./keys.js');
 const consumerKey = twitterKeys.consumer_key;
 const consumerSecret = twitterKeys.consumer_secret;
 const accessKey = twitterKeys.access_token_key;
 const accessSecret = twitterKeys.access_token_secret;
 const argv = process.argv[2];
+
+request('https://api.twitter.com/1.1/search/tweets.json?q=%40TrevorLMeadows', function (err, response, body) {
+    if (err) {
+        return console.log("nope")
+    }
+    console.log(JSON.parse(response));
+});
